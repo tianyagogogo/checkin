e1c0b059cddfd003cb3fbd3d36bcb677d17b39f9
tianyagogogo <24786545+tianyagogogo@users.noreply.github.com>
2023-04-19 10:50:50 +0800

    Create aliyundrive-checkin.py

---
+++ aliyundrive-checkin.py
@@ -0,0 +1,54 @@
+import json
+import  requests
+import os
+from notify import send
+
+# 使用refresh_token更新access_token
+def update_token(refresh_token):
+    url = 'https://auth.aliyundrive.com/v2/account/token'
+    data = {
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    response = requests.post(url=url, json=data).json()
+    access_token = response['access_token']
+    print('获取的access_token为{}'.format(access_token))
+    return access_token
+
+#签到
+def daily_check(access_token):
+    url = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
+    headers = {
+        'Authorization': access_token
+    }
+    response = requests.post(url=url, headers=headers, json={}).text
+    result = json.loads(response)
+    # print(result)
+    if 'success' in result:
+        print('签到成功')
+        for i, j in enumerate(result['result']['signInLogs']):
+            if j['status'] == 'miss':
+                day_json = result['result']['signInLogs'][i-1]
+                # print(day_json)
+                if not day_json['isReward']:
+                    content = '签到成功，今日未获得奖励'
+                else:
+                    content = '本月累计签到{}天,今日签到获得{}{}'.format(result['result']['signInCount'],
+                                                                     day_json['reward']['name'],
+                                                                     day_json['reward']['description'])
+                print(content)
+
+                return content
+
+
+def mian():
+    print('更新access_token')
+    refresh_token = os.environ["refresh_token"]
+    access_token = update_token(refresh_token)
+    print('更新成功，开始进行签到')
+    content = daily_check(access_token)
+    send('阿里云盘签到', content)
+
+
+if __name__ == '__main__':
+    mian()
